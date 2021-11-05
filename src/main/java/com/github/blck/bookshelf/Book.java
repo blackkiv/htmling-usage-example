03bbff81db4db763965e1fcf430efca2bce50c0a
blackkiv <fallena238@gmail.com>
2021-11-05 15:19:49 +0200

    new version

--- src/main/java/com/github/blck/bookshelf/Book.java
+++ src/main/java/com/github/blck/bookshelf/Book.java
@@ -2,24 +2,23 @@ package com.github.blck.bookshelf;
 
 import com.github.blck.htmling.annotation.Htmling;
 import com.github.blck.htmling.annotation.Ignore;
+import com.github.blck.htmling.annotation.Property;
 import com.github.blck.htmling.annotation.Style;
-import com.github.blck.htmling.annotation.Styles;
 import java.time.LocalDate;
 
-@Styles({
+@Htmling(getterPrefix = "", styles = {
     @Style(name = "background-color", value = "red"),
     @Style(name = "border", value = "medium dashed blue"),
     @Style(name = "font-style", value = "italic")
 })
-@Htmling(getterPrefix = "")
 public record Book(
-    @Style(name = "background-color", value = "orange")
+    @Property(styles = {@Style(name = "background-color", value = "orange")})
     String title,
-    @Style(name = "background-color", value = "yellow")
+    @Property(styles = {@Style(name = "background-color", value = "yellow")})
     String description,
-    @Style(name = "background-color", value = "green")
+    @Property(styles = {@Style(name = "background-color", value = "green")})
     LocalDate releasedAt,
-    @Style(name = "background-color", value = "cyan")
+    @Property(styles = {@Style(name = "background-color", value = "cyan")})
     int pages,
     @Ignore String secretInfo
 ) {
